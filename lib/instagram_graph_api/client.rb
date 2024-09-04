79770dbc0ac3a3d0f0cf78759b170081481881da
Kyle Bolton <kyle@digits.com>
2024-09-03 21:36:13 -0400

    Set api version to v20.0

--- lib/instagram_graph_api/client.rb
+++ lib/instagram_graph_api/client.rb
@@ -4,9 +4,10 @@ module InstagramGraphApi
   class Client < Koala::Facebook::API
     Dir[File.expand_path('../client/*.rb', __FILE__)].each{|f| require f}
 
+    Koala.config.api_version = "v20.0"
+
     include InstagramGraphApi::Client::Users
     include InstagramGraphApi::Client::Media
     include InstagramGraphApi::Client::Discovery
-
   end
 end
